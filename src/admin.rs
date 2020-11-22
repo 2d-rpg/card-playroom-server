e1cbf6dc6c0b67de5e58dd1872a1a73299e6fa89
bana118 <bana.titech@gmail.com>
2020-11-22 06:10:30 +0000

    add card upload page

--- src/admin.rs
+++ src/admin.rs
@@ -9,7 +9,18 @@ async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
+async fn upload_page(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let ctx = tera::Context::new();
+    let view = tmpl
+        .render("upload.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+}
+
 pub fn register(config: &mut web::ServiceConfig) {
     let templates = Tera::new("templates/**/*").unwrap();
-    config.data(templates).route("/", web::get().to(index));
+    config
+        .data(templates)
+        .route("/", web::get().to(index))
+        .route("/upload", web::get().to(upload_page));
 }
