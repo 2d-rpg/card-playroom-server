975a567681f9e38e08df95c0455a952f4a57b90d
bana118 <bana.titech@gmail.com>
2020-11-22 16:13:20 +0000

    add image upload comfirm message

--- src/admin.rs
+++ src/admin.rs
@@ -14,25 +14,56 @@ async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
 }
 
 async fn upload_page(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
-    let ctx = tera::Context::new();
+    let mut ctx = tera::Context::new();
+    ctx.insert("face_upload_comfirm", &"".to_owned());
+    ctx.insert("back_upload_comfirm", &"".to_owned());
     let view = tmpl
         .render("upload.html", &ctx)
         .map_err(|e| error::ErrorInternalServerError(e))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
-async fn upload_face(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
-    let ctx = tera::Context::new();
-    let view = tmpl
-        .render("upload.html", &ctx)
-        .map_err(|e| error::ErrorInternalServerError(e))?;
-    Ok(HttpResponse::Ok().content_type("text/html").body(view))
-}
-
-async fn upload_back(mut payload: Multipart) -> Result<HttpResponse, Error> {
+async fn upload_face(
+    tmpl: web::Data<tera::Tera>,
+    mut payload: Multipart,
+) -> Result<HttpResponse, Error> {
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let filename = content_type.get_filename().unwrap();
+        // TODO ファイル名重複防止
+        let filepath = format!("assets/face/{}", sanitize_filename::sanitize(&filename));
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    let mut ctx = tera::Context::new();
+    ctx.insert(
+        "face_upload_comfirm",
+        &"アップロードしました！！".to_owned(),
+    );
+    ctx.insert("back_upload_comfirm", &"".to_owned());
+    let view = tmpl
+        .render("upload.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+}
+
+async fn upload_back(
+    tmpl: web::Data<tera::Tera>,
+    mut payload: Multipart,
+) -> Result<HttpResponse, Error> {
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        // TODO ファイル名重複防止
         let filepath = format!("assets/back/{}", sanitize_filename::sanitize(&filename));
         // File::create is blocking operation, use threadpool
         let mut f = web::block(|| std::fs::File::create(filepath))
@@ -46,7 +77,16 @@ async fn upload_back(mut payload: Multipart) -> Result<HttpResponse, Error> {
             f = web::block(move || f.write_all(&data).map(|_| f)).await?;
         }
     }
-    Ok(HttpResponse::Found().header("Location", "/upload").finish())
+    let mut ctx = tera::Context::new();
+    ctx.insert("face_upload_comfirm", &"".to_owned());
+    ctx.insert(
+        "back_upload_comfirm",
+        &"アップロードしました！！".to_owned(),
+    );
+    let view = tmpl
+        .render("upload.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
 pub fn register(config: &mut web::ServiceConfig) {
