03f9180485d08b32a896f17752af9e127e21229f
bana118 <bana.titech@gmail.com>
2020-12-12 09:33:03 +0000

    add debugger extension and impl deck edit funtion in server side

--- src/edit_deck.rs
+++ src/edit_deck.rs
@@ -2,18 +2,15 @@
  * デッキ編集処理
  * http://localhost:8080/edit-deck
  */
-use crate::models::{Belonging, Card, Deck, NewBelonging, NewCard, NewDeck};
+use crate::models::{Belonging, Card, Deck, NewBelonging};
 use crate::schema::{belongings, cards, decks};
-use actix_files::Files;
 use actix_multipart::Multipart;
 use actix_web::{error, web, Error, HttpRequest, HttpResponse};
 use diesel::pg::expression::dsl::any;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use futures::{StreamExt, TryStreamExt};
-use serde::{Deserialize, Serialize};
-use std::fs;
-use std::io::Write;
+use serde::Serialize;
 use std::str;
 use tera::Tera;
 
@@ -82,11 +79,12 @@ async fn view_edit_deck_screen(
             .filter(cards::id.eq(any(card_ids_in_selected_deck)))
             .load::<Card>(&conn)
             .expect("Error loading cards");
-        let card_nums_in_selected_deck = Belonging::belonging_to(&selected_deck)
+        let mut card_nums_in_selected_deck = Belonging::belonging_to(&selected_deck)
             .select(belongings::num)
             .load::<i32>(&conn)
             .expect("Error loading belongings");
         let mut cards_info_in_selected_deck = Vec::new();
+        card_nums_in_selected_deck.reverse();
         for i in 0..cards_in_selected_deck.len() {
             let card_in_selected_deck = cards_in_selected_deck.pop().unwrap();
             let card_info_in_selected_deck = CardInfoInDeck {
@@ -97,6 +95,7 @@ async fn view_edit_deck_screen(
             };
             cards_info_in_selected_deck.push(card_info_in_selected_deck);
         }
+        cards_info_in_selected_deck.reverse();
         let inserted_ctx = insert_to_ctx(
             &mut ctx,
             cards,
@@ -113,99 +112,128 @@ async fn view_edit_deck_screen(
     }
 }
 
-async fn edit_deck(
+struct CardIdAndNum {
+    card_id: i32,
+    num: i32,
+}
+
+async fn complete_deck_editing(
     pool: web::Data<r2d2::Pool<ConnectionManager<PgConnection>>>,
     tmpl: web::Data<tera::Tera>,
     mut payload: Multipart,
 ) -> Result<HttpResponse, Error> {
-    let mut action: String = String::from("");
-    let mut card_ids: Vec<i32> = Vec::new();
-    let mut deck_id: i32 = -1;
+    let mut cards_id_and_num: Vec<CardIdAndNum> = Vec::new();
     let mut selected_deck_id: i32 = -1;
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let name = content_type.get_name().unwrap();
-        if name == "action" {
-            while let Some(chunk) = field.next().await {
-                let data = chunk.unwrap();
-                action = str::from_utf8(&data).unwrap().to_string();
-            }
-        } else if name == "card_id[]" {
-            while let Some(chunk) = field.next().await {
-                let data = chunk.unwrap();
-                let card_id = str::from_utf8(&data).unwrap().to_string();
-                card_ids.push(card_id.parse().unwrap());
-            }
-        } else if name == "deck_id" {
-            while let Some(chunk) = field.next().await {
-                let data = chunk.unwrap();
-                deck_id = str::from_utf8(&data).unwrap().parse().unwrap();
-            }
-        } else if name == "selected_deck_id" {
+        if name == "selected_deck_id" {
             while let Some(chunk) = field.next().await {
                 let data = chunk.unwrap();
                 selected_deck_id = str::from_utf8(&data).unwrap().parse().unwrap();
             }
+        } else {
+            while let Some(chunk) = field.next().await {
+                let card_id = name.parse().unwrap();
+                let data = chunk.unwrap();
+                let num = str::from_utf8(&data).unwrap().to_string().parse().unwrap();
+                let card_id_and_num = CardIdAndNum {
+                    card_id: card_id,
+                    num: num,
+                };
+                cards_id_and_num.push(card_id_and_num);
+            }
         }
     }
     let conn = pool.get().expect("couldn't get db connection from pool");
-    let mut ctx = tera::Context::new();
-    if action == "copy" {
-        for card_id in card_ids {
+    for card_id_and_num in cards_id_and_num {
+        let belonging = belongings::table
+            .filter(
+                belongings::deck_id
+                    .eq(selected_deck_id)
+                    .and(belongings::card_id.eq(card_id_and_num.card_id)),
+            )
+            .first::<Belonging>(&conn);
+        if belonging.is_err() {
             let new_belonging = NewBelonging {
-                deck_id: deck_id,
-                card_id: card_id,
-                num: 1,
+                deck_id: selected_deck_id,
+                card_id: card_id_and_num.card_id,
+                num: card_id_and_num.num,
             };
             diesel::insert_into(belongings::table)
                 .values(&new_belonging)
                 .execute(&conn)
                 .unwrap();
-        }
-    } else if action == "delete" {
-        for card_id in card_ids {
-            diesel::delete(
-                belongings::table.filter(
-                    belongings::deck_id
-                        .eq(selected_deck_id)
-                        .and(belongings::card_id.eq(card_id)),
-                ),
-            )
-            .execute(&conn)
-            .unwrap();
+        } else {
+            if card_id_and_num.num <= 0 {
+                diesel::delete(
+                    belongings::table.filter(
+                        belongings::deck_id
+                            .eq(selected_deck_id)
+                            .and(belongings::card_id.eq(card_id_and_num.card_id)),
+                    ),
+                )
+                .execute(&conn)
+                .unwrap();
+            } else {
+                diesel::update(
+                    belongings::table.filter(
+                        belongings::deck_id
+                            .eq(selected_deck_id)
+                            .and(belongings::card_id.eq(card_id_and_num.card_id)),
+                    ),
+                )
+                .set(belongings::num.eq(card_id_and_num.num))
+                .execute(&conn)
+                .unwrap();
+            }
         }
     }
+    let mut ctx = tera::Context::new();
+    let cards = cards::table
+        .load::<Card>(&conn)
+        .expect("Error loading cards");
     let decks = decks::table
         .load::<Deck>(&conn)
         .expect("Error loading decks");
-    if selected_deck_id == -1 {
-        let cards = cards::table
-            .load::<Card>(&conn)
-            .expect("Error loading cards");
-        ctx.insert("cards", &cards);
-        ctx.insert("decks", &decks);
-        ctx.insert("add_deck_confirm", &"".to_owned());
-        ctx.insert("deck_name", &"すべてのカード".to_owned());
-        ctx.insert("deck_id", &selected_deck_id.to_owned());
-    } else {
-        let selected_deck = decks::table
-            .find(selected_deck_id)
-            .first::<Deck>(&conn)
-            .expect("Error loading deck");
-        let card_ids_in_selected_deck =
-            Belonging::belonging_to(&selected_deck).select(belongings::card_id);
-        let cards = cards::table
-            .filter(cards::id.eq(any(card_ids_in_selected_deck)))
-            .load::<Card>(&conn)
-            .expect("Error loading cards");
-        ctx.insert("cards", &cards);
-        ctx.insert("decks", &decks);
-        ctx.insert("add_deck_confirm", &"".to_owned());
-        ctx.insert("deck_name", &selected_deck.name.to_owned());
-        ctx.insert("deck_id", &selected_deck_id.to_owned());
+    let selected_deck = decks::table
+        .find(selected_deck_id)
+        .first::<Deck>(&conn)
+        .expect("Error loading deck");
+    let card_ids_in_selected_deck =
+        Belonging::belonging_to(&selected_deck).select(belongings::card_id);
+    let mut cards_in_selected_deck = cards::table
+        .filter(cards::id.eq(any(card_ids_in_selected_deck)))
+        .load::<Card>(&conn)
+        .expect("Error loading cards");
+    let mut card_nums_in_selected_deck = Belonging::belonging_to(&selected_deck)
+        .select(belongings::num)
+        .load::<i32>(&conn)
+        .expect("Error loading belongings");
+    let mut cards_info_in_selected_deck = Vec::new();
+    card_nums_in_selected_deck.reverse();
+    for i in 0..cards_in_selected_deck.len() {
+        let card_in_selected_deck = cards_in_selected_deck.pop().unwrap();
+        let card_info_in_selected_deck = CardInfoInDeck {
+            card_id: card_in_selected_deck.id,
+            face: card_in_selected_deck.face,
+            back: card_in_selected_deck.back,
+            num: card_nums_in_selected_deck[i],
+        };
+        cards_info_in_selected_deck.push(card_info_in_selected_deck);
     }
+    cards_info_in_selected_deck.reverse();
+    let inserted_ctx = insert_to_ctx(
+        &mut ctx,
+        cards,
+        true,
+        decks,
+        Some(selected_deck),
+        "デッキ編集完了",
+        cards_info_in_selected_deck,
+    );
     let view = tmpl
-        .render("deck.html", &ctx)
+        .render("edit-deck.html", &inserted_ctx)
         .map_err(|e| error::ErrorInternalServerError(e))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
@@ -215,5 +243,6 @@ pub fn register(config: &mut web::ServiceConfig) {
     config
         .data(templates)
         .route("/edit-deck", web::get().to(view_edit_deck_screen))
-        .route("/edit-deck/{deck_id}", web::get().to(view_edit_deck_screen));
+        .route("/edit-deck/{deck_id}", web::get().to(view_edit_deck_screen))
+        .route("/edit-deck/complete", web::post().to(complete_deck_editing));
 }
